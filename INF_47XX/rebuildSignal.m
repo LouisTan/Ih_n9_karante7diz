a5dde7ab70e46b411e2cd708a477dd394b2e890b
Louis Tan-Therrien <louis.tant@gmail.com>
2017-09-06 12:16:17 -0700

    signal2image corrige (reformer la matrice)

--- INF_47XX/rebuildSignal.m
+++
@@ -1,7 +0,0 @@
-function [signal_real] = rebuildSignal(signal_predict,signal_error)
-    assert(numel(signal_predict)==numel(signal_error));
-    signal_real = zeros(numel(signal_predict),1,'uint8');
-    for n=1:numel(signal_predict)
-        signal_real(n) = uint8(double(signal_predict(n))+double(signal_error(n)));
-    end
-end

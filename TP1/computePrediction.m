bcc8a0abc48ae579bef9f70cce91afe42aaf6d6c
Lester <louis.tant@gmail.com>
2017-09-17 01:38:26 -0400

    fonction computePrediction termine

--- TP1/computePrediction.m
+++ TP1/computePrediction.m
@@ -8,19 +8,40 @@ function [signal_predict] = computePrediction(image_base,complicationID)
         % note: the produced prediction signal should correspond to the output of image2signal(...)
         if(strcmp(complicationID,'blur'))
             % TODO: IMPROVE ME! @@@@@
-            signal_predict = image2signal(image_base,complicationID);
+            %https://www.mathworks.com/help/images/ref/imgaussfilt.html
+            image_base = imgaussfilt(image_base, 2);
+            signal_predict = image2signal(image_base,2);
             assert(numel(signal_predict)==numel(image_base));
         elseif(strcmp(complicationID,'dark'))
             % TODO: IMPROVE ME! @@@@@
+            %https://www.mathworks.com/help/images/ref/imadjust.html
+            image_base = imajust(image_base)
             signal_predict = image2signal(image_base,complicationID);
             assert(numel(signal_predict)==numel(image_base));
         elseif(strcmp(complicationID,'noisy'))
             % TODO: IMPROVE ME! @@@@@
+            %https://www.mathworks.com/help/images/ref/medfilt2.html
+            if ndims(image_base) == 2
+                image_base = medlift2(image_base);
+            elseif ndims(image_base) == 3
+                %https://www.mathworks.com/help/images/ref/medfilt3.html
+                image_base = medfilt3(image_base);
+            else disp('This case is not covered (computePrediction)');
             signal_predict = image2signal(image_base,complicationID);
             assert(numel(signal_predict)==numel(image_base));
+            end
         elseif(strcmp(complicationID,'colorShift'))
             assert(size(image_base,3)==3); % assume we are processing a 3-channel image
             % TODO: IMPROVE ME! @@@@@
+            % https://www.mathworks.com/matlabcentral/answers/111104-how-to-separate-an-image-to-rgb
+            % Extract the individual red, green, and blue color channels.
+            redChannel = image_base(:, :, 1);
+            greenChannel = image_base(:, :, 2);
+            blueChannel = image_base(:, :, 3);
+            
+            % Recombine separate color channels into a single, true color RGB image.
+            image_base = cat(3, greenChannel,redChannel, blueChannel);
+            
             signal_predict = image2signal(image_base,complicationID);
             assert(numel(signal_predict)==numel(image_base));
         else

d2fc5c162db5684fe52f0930a790040c59ee4181
Lester <louis.tant@gmail.com>
2017-09-26 09:17:50 -0400

    conv_rgb_yCbCr complete (non-teste)

--- TP2/conv_rgb2ycbcr.m
+++ TP2/conv_rgb2ycbcr.m
@@ -1,7 +1,7 @@
 % conv_rgb2ycbcr: converts an 8-bit-depth RGB image to Y'CbCr format using optional 4:2:0 subsampling
 function [Y, Cb, Cr] = conv_rgb2ycbcr(RGB, subsample)
     assert(numel(RGB)>0 && size(RGB,3)==3);
-    Y = zeros(size(RGB,1),size(RGB,2),'uint8');
+    %Y = zeros(size(RGB,1),size(RGB,2),'uint8');
     
     R = RGB(:,:,1);
     G = RGB(:,:,2);
@@ -10,37 +10,47 @@ function [Y, Cb, Cr] = conv_rgb2ycbcr(RGB, subsample)
     img_s = size(RGB);
     
     RGB_420 = [];
+    RGB_444 = [];
     
-    p = 1:img_s(3);
-    n = 1:2:img_s(1);
-    m = 1:2:img_s(2);
+    Y = [];
+    Cb = [];
+    Cr = [];
     
-    z = 1;
-    for k = p
-        y = 1;
-        for j = m
-            x = 1;
-            for i = n
-                RGB_420(x,y,z) = RGB(i,j,k);
-                x = x + 1;
-            end
-        y = y + 1;    
-        end
-    z = z + 1;
-    end
+    %4:4:4
+    if subsample
+        Y = 0.299*R + 0.587*G + 0.114*B;
+        Cb = 128 + 0.564*(B - Y);
+        Cr = 128 + 0.713*(R - Y); 
+    
+        %disp(RGB_424);
+        disp(Y);
+        disp(Cb);
+        disp(Cr);
+    %4:2:0   
+    else
+        p = 1:img_s(3);
+        n = 1:2:img_s(1);
+        m = 1:2:img_s(2);
 
-    disp(RGB_test);
-            
-    
-    
-%     %4:4:4
-%     if subsample
-%         Cb = zeros(...,'uint8');
-%         Cr = zeros(...,'uint8');
-%         
-%     %4:2:0   
-%     else
-%         Cb = zeros(...,'uint8');
-%         Cr = zeros(...,'uint8');   
-%     end
+        z = 1;
+        for k = p
+            y = 1;
+            for j = m
+                x = 1;
+                for i = n
+                    RGB_420(x,y,z) = RGB(i,j,k);
+                    x = x + 1;
+                end
+            y = y + 1;    
+            end
+        z = z + 1;
+        end
+        Y = 0.299*R + 0.587*G + 0.114*B;
+        Cb = 128 + 0.564*(B - Y);
+        Cr = 128 + 0.713*(R - Y);
+        
+        disp(Y);
+        disp(Cb);
+        disp(Cr);
+    end
 end

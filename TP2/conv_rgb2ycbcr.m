aabd6e6f6527f5e9f40cb9e53f95611b4731883c
Lester <louis.tant@gmail.com>
2017-09-28 16:28:23 -0400

    Meilleure implementation de conv_rgb2ycbcr

--- TP2/conv_rgb2ycbcr.m
+++ TP2/conv_rgb2ycbcr.m
@@ -5,50 +5,45 @@ function [Y, Cb, Cr] = conv_rgb2ycbcr(RGB, subsample)
    
     img_s = size(RGB);
     
-    RGB_420 = [];
-    RGB_444 = [];
+    YCbCr = zeros(3);
     
-    R = [];
-    G = [];
-    B = [];
+    R = RGB(:,:,1);
+    G = RGB(:,:,2);
+    B = RGB(:,:,3);
     
-    %4:4:4
+    %4:4:2
     if subsample
-        Y = 0.299*R + 0.587*G + 0.114*B;
-        Cb = 128 + 0.564*(B - Y);
-        Cr = 128 + 0.713*(R - Y);
-    
-        %disp(YCbCr_444);
-        %disp(Y);
-        %disp(Cb);
-        %disp(Cr);
-    %4:2:0   
+        p = 1:img_s(3);
+        n = 1:img_s(1);
+        m = 1:img_s(2);
+    %4:4:4  
     else
         p = 1:img_s(3);
         n = 1:2:img_s(1);
         m = 1:2:img_s(2);
-
-        z = 1;
-        for k = p
-            y = 1;
-            for j = m
-                x = 1;
-                for i = n
-                    YCbCr_420(x,y,z) = RGB(i,j,k);
-                    x = x + 1;
-                end
-            y = y + 1;    
-            end
-        z = z + 1;
-        end
-        
-        Y = 0.299*R + 0.587*G + 0.114*B;
-        Cb = 128 + 0.564*(B - Y);
-        Cr = 128 + 0.713*(R - Y);
-        
-        %disp(YCbCr_420);
-        %disp(Y);
-        %disp(Cb);
-        %disp(Cr);
     end
+    
+    z = 1;
+    for k = p
+        y = 1;
+        for j = m
+            x = 1;
+            for i = n
+                YCbCr(x,y,z) = RGB(i,j,k);
+                x = x + 1;
+            end
+        y = y + 1;    
+        end
+    z = z + 1;
+    end
+    
+    Y = 0.299*R + 0.587*G + 0.114*B;
+    Cb = 128 + 0.564*(B - Y);
+    Cr = 128 + 0.713*(R - Y);
+
+    %disp(YCbCr);
+    
+    compr_rate = 1.0 - (numel(YCbCr)./(numel(RGB)));
+    fprintf('\t... compression rate = %f\n',compr_rate);
+    
 end

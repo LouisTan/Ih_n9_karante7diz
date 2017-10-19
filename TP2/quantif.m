e2d58294b4e7bbe7c00cc3ea637a68050adfc0e9
Lester <louis.tant@gmail.com>
2017-10-19 03:03:15 -0400

    Supression testbenc.m

--- TP2/quantif.m
+++ TP2/quantif.m
@@ -8,8 +8,8 @@ function [block_quantif] = quantif(block,quality)
         reshape([3,2,2,3,5,8,10,12,2,2,3,4,5,12,12,11,3,3,3,5,8,11,14,11,3,3,4,6,10,17,16,12,4,4,7,11,14,22,21,15,5,7,11,13,16,12,23,18,10,13,16,17,21,24,24,21,14,18,19,20,22,20,20,20],[8,8]),...
         ones(8,8),...
     };
-    closest_quality = quant_mat_qualities(1);
-    closest_quant_mat = quant_mats{1};
+    closest_quality = quant_mat_qualities(4);
+    closest_quant_mat = quant_mats{4};
     for quality_idx=2:numel(quant_mat_qualities)
         if abs(quality-quant_mat_qualities(quality_idx))<abs(quality-closest_quality)
             closest_quality = quant_mat_qualities(quality_idx);
